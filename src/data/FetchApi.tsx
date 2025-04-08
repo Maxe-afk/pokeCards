f55a9a7ae19de7eb575a299e23e1f95c21e11be2
Jules Clauwaert <jules.clauwaert@gmail.com>
2025-04-08 12:19:12 +0200

    added some css

--- src/data/FetchApi.tsx
+++ src/data/FetchApi.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { PokemonCard } from "../model/PokemonCard";
+import Styles from "../data/FetchApi.module.css";
 
 const pokemonCards: PokemonCard[] = [
   new PokemonCard("Salamèche", "Salamèche.png", 150),
 ];
+
+
+
+
 
 export const FetchApi = () => {
   const [cards, setCards] = useState(pokemonCards);
@@ -44,6 +49,25 @@ export const FetchApi = () => {
     fetchData();
   }, []);
 
+  const [hoveredCard, setHoveredCard] = useState<string | null>(null);
+  const [hoveredFavorite, setHoveredFavorite] = useState<string | null>(null);
+
+  function handleMouseEnter(cardName: string) {
+    setHoveredCard(cardName);
+  }
+
+  function handleMouseLeave() {
+    setHoveredCard(null);
+  }
+
+  function handleFavoriteMouseEnter(cardName: string) {
+    setHoveredFavorite(cardName);
+  }
+
+  function handleFavoriteMouseLeave() {
+    setHoveredFavorite(null);
+  }
+
   const toggleFavorite = (card: PokemonCard) => {
     setFavorites((prevFavorites) => {
       if (prevFavorites.some((fav) => fav.name === card.name)) {
@@ -60,32 +84,69 @@ export const FetchApi = () => {
   if (error) return <p>Erreur : {error}</p>;
 
   return (
-    <div className="App">
-      <h1>Données récupérées :</h1>
+    <div className={Styles.all}>
+      <div className={Styles.box1}>
+      <h2 className={Styles.cardName}>Données récupérées :</h2>
+      <div className={Styles.cardsContainer}>
+        
       {cards.length > 0 &&
-        cards.map((card) => (
-          <div
-            key={card.name}
-            onClick={() => toggleFavorite(card)}
-          >
-            <h2>{card.name}</h2>
-            <img src={card.picture} alt={card.name} />
-            <p>{card.price}€</p>
-          </div>
-        ))}
+  cards.map((card) => (
+    <div
+      key={card.name}
+      onClick={() => toggleFavorite(card)} className={Styles.card}>
+    
+      
+      <img
+        onMouseEnter={() => handleMouseEnter(card.name)}
+        onMouseLeave={handleMouseLeave}
+        className={`${Styles.img} ${Styles.imgCursor} ${
+          favorites.some((fav) => fav.name === card.name) ? Styles.inFavorites : ''
+        }`}
+        src={card.picture}
+        alt={card.name}
+      />
+      <div className={`${Styles['description']} ${
+          hoveredCard === card.name ? Styles['visible'] : ''
+        }`}>
 
-      <h2>Favoris :</h2>
-      {favorites.length > 0 ? (
-        favorites.map((fav) => (
-          <div key={fav.name}>
-            <h2>{fav.name}</h2>
-            <img src={fav.picture} alt={fav.name} />
-            <p>{fav.price}€</p>
-          </div>
-        ))
-      ) : (
-        <p>Aucun favori pour le moment.</p>
-      )}
+      <h2>{card.name}</h2>
+      <p>{card.price}€</p>
+
+      </div>
+      
+    </div>
+  ))}
+      </div>
+      </div>
+      <div className={Styles.box2}>
+<h2>Favoris :</h2>
+      <div className={Styles.favoriteContainer}>
+        {favorites.length > 0 ? (
+          favorites.map((fav) => (
+            <div
+              key={fav.name}
+              className={Styles.card}
+              onMouseEnter={() => handleFavoriteMouseEnter(fav.name)}
+              onMouseLeave={handleFavoriteMouseLeave}
+            >
+              <img
+                className={Styles.img}
+                src={fav.picture}
+                alt={fav.name}
+              />
+              <div className={`${Styles['description']} ${
+                hoveredFavorite === fav.name ? Styles['visible'] : ''
+              }`}>
+                <h2>{fav.name}</h2>
+                <p>{fav.price}€</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Aucun favori pour le moment.</p>
+        )}
+      </div>
+    </div>
     </div>
   );
 };
